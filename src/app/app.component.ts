fa1eb98781556a26efa29a0c980b14c3d30f38c8
angelinim <angelinim62@gmail.com>
2020-08-31 19:48:10 -0400

    remade sidenav. added icons and made responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'study-notes-front-end';
+  innerWidth;
+  isCollapsed = false;
+
+  constructor(private sideNav: ElementRef, private content: ElementRef){
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth < 768){
+      this.isCollapsed = true;
+      this.fixSideNav(1);
+    }
+    else{
+      this.isCollapsed = false;
+      this.fixSideNav(0);
+    }
+  }
+
+  fixSideNav(fixed: number){
+    let nav = this.sideNav.nativeElement.querySelector(`#nav`);
+    let content = this.sideNav.nativeElement.querySelector(`#content`);
+
+    if(fixed === 0){
+      nav.style.position = "unset";
+      content.style.margin= "0 0 0 0";
+    }
+    else{
+      nav.style.position = "fixed";
+      content.style.margin= "0 0 0 36px";
+    }
+  }
+
+
+  ngOnInit(): void{
+    this.onResize();
+  }
 }
